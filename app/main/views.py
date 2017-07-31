ea2fce434fe30838b8fd78d08c3f813d616a28d6
kaishuibaicai <a272251416@gmail.com>
2017-07-31 20:56:13 +0800

    16b

--- app/main/views.py
+++ app/main/views.py
@@ -233,6 +233,6 @@ def server_shutdown():
 @main.after_app_request
 def after_request(response):
     for query in get_debug_queries():
-        if query.duration >= current_app.config['FLASK_SLOW_DB_QUERY_TIME']:
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
             current_app.logger.warning('Slow query: %s\nDuration: %fs\nContext: %s\n' % (query.statement, query.parameters, query.duration, query.context))
     return response
