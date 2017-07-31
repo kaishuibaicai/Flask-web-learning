0a705915132ce3ab8bb93a47fc62140c861ac2ab
kaishuibaicai <a272251416@gmail.com>
2017-07-31 16:48:18 +0800

    15d

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
+    return render_template('403.html'), 403
 
 @main.app_errorhandler(404)
 def page_not_found(e):
@@ -13,4 +21,8 @@ def page_not_found(e):
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
     return render_template('500.html'), 500
